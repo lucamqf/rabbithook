29d88ad4395718564ab5edbae70e9c99c4dd74f4
Luigi <luigi.jackson@gmail.com>
2024-04-20 16:56:31 -0300

    feat: add useAsync hook

---
+++ packages/registry/src/hooks/useAsync.js
@@ -0,0 +1,23 @@
+import { useCallback, useState, useEffect } from "react"
+
+function useAsync(callback, dependencies = []) {
+	const [loading, setLoading] = useState(true)
+	const [error, setError] = useState(null)
+	const [value, setValue] = useState(null)
+
+	const callbackMemoized = useCallback(() => {
+		setLoading(true)
+		setError(null)
+		setValue(null)
+		callback()
+			.then(setValue)
+			.catch(setError)
+			.finally(() => setLoading(false))
+	}, dependencies)
+
+	useEffect(() => {
+		callbackMemoized()
+	}, [callbackMemoized])
+
+	return { loading, error, value }
+}
