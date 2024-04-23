9651ce5a20cf6298980fb32be327f569211e0b83
Luigi <luigi.jackson@gmail.com>
2024-04-23 09:40:43 -0300

    feat: add useMousePosition hook

---
+++ packages/registry/src/hooks/useMousePosition.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+function useMousePosition() {
+  const [mousePosition, setMousePosition] = useState({});
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setMousePosition({ x: event.clientX, y: event.clientY });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  return mousePosition;
+}
+
+export default useMousePosition;
