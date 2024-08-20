acff333eb15f2c290008bf1816fbc0eb45ce2afc
Luca <lmqf@proton.me>
2024-08-20 19:14:44 -0300

    feat: add hook useThrottledValue

---
+++ packages/registry/src/hooks/use-throttled-value/index.ts
@@ -0,0 +1,27 @@
+import { useEffect, useRef, useState } from 'react';
+
+function useThrottledValue<T>(value: T, delay: number): T {
+  const [throttledValue, setThrottledValue] = useState(value);
+  const lastExecutedRef = useRef(0);
+
+  useEffect(() => {
+    const now = Date.now();
+    const timeSinceLastExecution = now - lastExecutedRef.current;
+
+    if (timeSinceLastExecution >= delay) {
+      setThrottledValue(value);
+      lastExecutedRef.current = now;
+    } else {
+      const timeoutId = setTimeout(() => {
+        setThrottledValue(value);
+        lastExecutedRef.current = Date.now();
+      }, delay - timeSinceLastExecution);
+
+      return () => clearTimeout(timeoutId);
+    }
+  }, [value, delay]);
+
+  return throttledValue;
+}
+
+export default useThrottledValue;
